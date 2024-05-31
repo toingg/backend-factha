b9328eec97e4cad34c511983963daf4bf34fb297
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-05-31 20:28:37 +0700

    This is code for cloud environment in google cloud

--- src/server/books.js
+++
@@ -1,5 +0,0 @@
-const books = [
-
-];
-
-module.exports = books;
--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
   const server = Hapi.server({
     port: 3000,
-    host: "localhost",
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
