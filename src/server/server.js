5f56d0174baf357e1a02a3bd7a750e764b261412
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-06-08 11:44:42 +0700

    Second deploy

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const InputError = require("../exceptions/InputError");
 (async () => {
   const server = Hapi.server({
     port: process.env.PORT,
-    host: "localhost",
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
